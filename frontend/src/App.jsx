9d51f14024c9035363660a2fee5357afda022588
owhevle <tomeobj26@gmail.com>
2025-12-10 15:47:44 +0800

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import FloorsBuildingBlueprint from "../components/FloorsBuildingBlueprint"
+const App = () => {
+  return (
+    <div>
+      <FloorsBuildingBlueprint/>
+    </div>
+  )
+}
+
+export default App
